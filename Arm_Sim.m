264d3d6b90bd80b742a0658955ed0f05c3897149
alexnavtt <alex.j.navarro@outlook.com>
2019-10-16 18:10:03 -0500

    Added gradient descent

--- Arm_Sim.m
+++ Arm_Sim.m
@@ -2,8 +2,11 @@ clear
 close all
 
 %% Definition of Physical Parameters
+%Subscript s refers to shoulder
+%Subscript e refers to elbow
+%Subscript h refers to hand
 
-global ms me mh ls le lh lms lme lmh Is Ie Ih qstar
+global ms me mh ls le lh lms lme lmh Is Ie Ih qstar count
 
 %masses of arm segments (kg)
 ms = 1.93;
@@ -26,12 +29,12 @@ Ie = 0.0188;
 Ih = 0.0003;
 
 %joint limits
-qs_plus = deg2rad(90);
-qs_minus = deg2rad(-20);
-qe_plus = deg2rad(170);
+qs_plus = deg2rad(30);
+qs_minus = deg2rad(-60);
+qe_plus = deg2rad(150);
 qe_minus = deg2rad(0);
-qh_plus = deg2rad(270);
-qh_minus = deg2rad(60);
+qh_plus = deg2rad(90);
+qh_minus = deg2rad(-75);
 qstar = 0.5*[qs_plus+qs_minus;qe_plus+qe_minus;qh_plus+qh_minus];
 
 %% State Definition
@@ -40,15 +43,12 @@ qstar = 0.5*[qs_plus+qs_minus;qe_plus+qe_minus;qh_plus+qh_minus];
 q0 = deg2rad([0;120;75]);
 qs0 = q0(1); qe0 = q0(2); qh0 = q0(3);
 
-%Starting end point position
-x = ls*cos(qs0) + le*cos(qs0+qe0) + lh*cos(qs0+qe0+qh0);
-y = ls*sin(qs0) + le*sin(qs0+qe0) + lh*sin(qs0+qe0+qh0);
-
 count = 0;
+figure(1)
+hold on
 [TOUT, POSITION] = ode45(@qdot, [0 1], q0); %solve for end point over time
 
-%figure(1)
-%plotArm(q0)
+%% Inertial Model
 
 function H = massMat(q)
 
@@ -70,6 +70,8 @@ H(3,2) = H(2,3);
 H(3,3) = Ih + mh*lmh^2;
 end
 
+%% Jacobian Model
+
 function Jacobian = J(q)
 
 global ls le lh
@@ -85,6 +87,8 @@ Jacobian(2,3) = lh*cos(qs+qe+qh);
 
 end
 
+%% Cost Function and its derivative
+
 function delV = grad_V(q)
 
 global qstar
@@ -97,20 +101,23 @@ function cost = V(q)
 
 global qstar
 x = q(1); y = q(2); z = q(3);
-cost = (x-q_star(1))^2 + (y - qstar(2))^2 + (z - qstar(3))^2;
+cost = (x-qstar(1))^2 + (y - qstar(2))^2 + (z - qstar(3))^2;
 
 end
 
+%% State Propagation Function
+
 function q_dot_star = qdot(t,q)
 
-qs = q(1); qe = q(2); qh = q(3);
-
+global count
 T = 1; %duration of movement
-A = 0.5; %amplitude of movement
+A = 0.4; %amplitude of movement
 
+%Bell shaped velocity profile
 velocity = ((t/T)^2 - 2*(t/T) + 1)*30*A*(t/T)^2/T;
-xydot = velocity*[cosd(30);sind(30)];
+xydot = velocity*[cosd(20);sind(20)];
 
+%Minimization parameters for kinetic energy
 JT = transpose(J(q));
 M = massMat(q);
 M_inv = inv(M);
@@ -118,16 +125,23 @@ M_inv = inv(M);
 J_pseudo = M_inv*JT*inv((J(q)*M_inv*JT));
 q_dot_star = J_pseudo*xydot;
 
-%steepest descent optimization
-syms X
-grad_q0 = grad_V(q);
--dot(grad_V(V(q-chi*grad_q0)),grad_q0);
+%Steepest descent optimization to avoid uncomfortable positions
+X = 0.5; %calculated by hand
+n = -grad_V(q)/norm(grad_V(q));
+delta_q_dot = -X*dot(grad_V(q),n)*n;
+q_dot_star = q_dot_star + delta_q_dot;
 
-hold on
-plotArm(q)
-%pause
+if count ==4
+    plotArm(q)
+    count = 0;
+    pause
+else
+    count = count +1;
+end
 
 end
+
+%% Plotting Arm Position
 
 function plotArm(q)
 
